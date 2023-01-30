d8511535373b59dc96f063827601dc1ac3ec379f
4noth1ng <1766695958@qq.com>
2023-01-30 17:49:25 +0800

    feat: init1

--- src/views/article-ranking/index.vue
+++ src/views/article-ranking/index.vue
@@ -50,16 +50,25 @@
 </template>
 
 <script setup>
-import { ref, onActivated } from 'vue'
-import { getArticleList } from '@/api/article'
+import { ref, onActivated, onMounted } from 'vue'
+import { getArticleList, articleSort, deleteArticle } from '@/api/article'
 import { watchSwitchLang } from '@/utils/i18n'
 import { dynamicData, selectDynamicLabel, tableColumns } from './dynamic'
+import { tableRef, initSortable } from './sortable'
+import { useI18n } from 'vue-i18n'
+import { useRouter } from 'vue-router'
 // 数据相关
 const tableData = ref([])
 const total = ref(0)
 const page = ref(1)
 const size = ref(10)
-
+/**
+ * 查看按钮点击事件
+ */
+const router = useRouter()
+const onShowClick = (row) => {
+  router.push(`/article/${row._id}`)
+}
 // 获取数据的方法
 const getListData = async () => {
   const result = await getArticleList({
@@ -89,6 +98,28 @@ const handleCurrentChange = (currentPage) => {
   page.value = currentPage
   getListData()
 }
+
+// 表格拖拽相关
+onMounted(() => {
+  initSortable(tableData, getListData)
+})
+// 删除用户
+const i18n = useI18n()
+const onRemoveClick = (row) => {
+  ElMessageBox.confirm(
+    i18n.t('msg.article.dialogTitle1') +
+      row.title +
+      i18n.t('msg.article.dialogTitle2'),
+    {
+      type: 'warning'
+    }
+  ).then(async () => {
+    await deleteArticle(row._id)
+    ElMessage.success(i18n.t('msg.article.removeSuccess'))
+    // 重新渲染数据
+    getListData()
+  })
+}
 </script>
 
 <style lang="scss" scoped>
@@ -109,7 +140,11 @@ const handleCurrentChange = (currentPage) => {
   :deep(.el-table__row) {
     cursor: pointer;
   }
-
+  :deep(.sortable-ghost) {
+    opacity: 0.6;
+    color: #fff !important;
+    background: #304156 !important;
+  }
   .pagination {
     margin-top: 20px;
     text-align: center;
